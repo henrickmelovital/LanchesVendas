cb3d6744ee01f7f80f47a875ef1cf8c73b16dd1c
Henrick Melo Vital <henrick.melo@yahoo.com.br>
2023-06-04 22:26:03 -0300

    Entity Framework Core 7 e a string de conexão

---
+++ MVC_2022/Context/AppDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MVC_2022.Models;
+
+namespace MVC_2022.Context
+{
+    public class AppDbContext : DbContext
+    {
+        protected AppDbContext(DbContextOptions<AppDbContext> options)
+            : base(options)
+        {
+
+        }
+
+        public DbSet<Categoria> Categorias { get; set; }
+        public DbSet<Lanche> Lanches { get; set; }
+    }
+}
---
+++ MVC_2022/Models/Lanche.cs
@@ -0,0 +1,18 @@
+﻿namespace MVC_2022.Models
+{
+    public class Lanche
+    {
+        public int LancheId { get; set; }
+        public string Nome { get; set; }
+        public string DescricaoCurta { get; set; }
+        public string DescricaoDetalhada { get; set; }
+        public decimal Preco { get; set; }
+        public string ImagemUrl { get; set; }
+        public string ImagemThumbnaiUrl { get; set; }
+        public bool IsLachePreferido { get; set; }
+        public bool EmEstoque { get; set; }
+
+        public int CategoriaId { get; set; }
+        public virtual Categoria Categoria { get; set; }
+    }
+}
