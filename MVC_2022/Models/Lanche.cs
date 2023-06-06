3dbbd6c79c463b10bb6ba651288976442f8edfb3
Henrick Melo Vital <henrick.melo@yahoo.com.br>
2023-06-06 15:29:56 -0300

    Aula01iacaoDoProjetoASPNETCOREMVC.txt

--- MVC_2022/Models/Lanche.cs
+++ MVC_2022/Models/Lanche.cs
@@ -1,9 +1,21 @@
-﻿namespace MVC_2022.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MVC_2022.Models
 {
+    [Table("Lanche")]
     public class Lanche
     {
         public int LancheId { get; set; }
+
+        [Required(ErrorMessage = "O nome do lanche deve ser informado")]
+        [Display(Name = "Nome do lanche")]
         public string Nome { get; set; }
+
+        [Required(ErrorMessage = "A descrição do lanche deve ser informada")]
+        [Display(Name = "Descrição do lanche")]
+        [MinLength(20, ErrorMessage = "Descrição deve ter no minimo {1} caracteres")]
+        [MaxLength(200, ErrorMessage = "Descrição não pode exceder {1} caracteres")]
         public string DescricaoCurta { get; set; }
         public string DescricaoDetalhada { get; set; }
         public decimal Preco { get; set; }
