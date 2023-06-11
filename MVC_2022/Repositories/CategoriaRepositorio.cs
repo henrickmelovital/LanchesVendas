da06a096d5c773d5b84361fb155f5e3a399a402c
Henrick Melo Vital <henrick.melo@yahoo.com.br>
2023-06-11 12:44:26 -0300

    Implementando o padrão repository para Categorias

---
+++ MVC_2022/Repositories/CategoriaRepositorio.cs
@@ -0,0 +1,18 @@
+﻿using MVC_2022.Context;
+using MVC_2022.Models;
+using MVC_2022.Repositories.Interface;
+
+namespace MVC_2022.Repositories
+{
+    public class CategoriaRepositorio : ICategoriaRepository
+    {
+        private readonly AppDbContext _context;
+
+        public CategoriaRepositorio(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public IEnumerable<Categoria> Categorias => _context.Categorias;
+    }
+}
---
+++ MVC_2022/Repositories/Interface/ICategoriaRepository.cs
@@ -0,0 +1,9 @@
+﻿using MVC_2022.Models;
+
+namespace MVC_2022.Repositories.Interface
+{
+    public interface ICategoriaRepository
+    {
+        IEnumerable<Categoria> Categorias { get; }
+    }
+}
