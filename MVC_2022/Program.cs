957749dd9deb55f5e32751c7fbcfb535cef138a9
Henrick Melo Vital <henrick.melo@yahoo.com.br>
2023-06-11 18:52:15 -0300

    // Registrando o serviço dos repositórios : Injeção de Dependência

--- MVC_2022/Program.cs
+++ MVC_2022/Program.cs
@@ -1,16 +1,23 @@
 using Microsoft.EntityFrameworkCore;
 using MVC_2022.Context;
+using MVC_2022.Repositories;
+using MVC_2022.Repositories.Interface;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
+// Conexão com o banco de dados
 var connectionString = builder.Configuration.GetConnectionString("LanchesMac") ??
     throw new InvalidOperationException("Connection string 'LanchesMac' not found.");
 
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlServer(connectionString));
+
+// Registrando o serviço dos repositórios : Injeção de Dependência
+builder.Services.AddTransient<ILancheRepository, LancheRepository>();
+builder.Services.AddTransient<ICategoriaRepository, CategoriaRepositorio>();
 
 var app = builder.Build();
 
