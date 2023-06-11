3f32aa3c994bb4833fe94a6472740d342d9d1101
Henrick Melo Vital <henrick.melo@yahoo.com.br>
2023-06-11 13:13:53 -0300

    implementando o padrão Repository para Lanches

---
+++ MVC_2022/Repositories/Interface/ILancheRepository.cs
@@ -0,0 +1,12 @@
+﻿using MVC_2022.Models;
+
+namespace MVC_2022.Repositories.Interface
+{
+    public interface ILancheRepository
+    {
+        IEnumerable<Lanche> Lanches { get; }
+        IEnumerable<Lanche> LanchesPreferidos { get; }
+
+        Lanche GetLancheById(int lancheid);
+    }
+}
---
+++ MVC_2022/Repositories/LancheRepository.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MVC_2022.Context;
+using MVC_2022.Models;
+using MVC_2022.Repositories.Interface;
+
+namespace MVC_2022.Repositories
+{
+    public class LancheRepository : ILancheRepository
+    {
+        private readonly AppDbContext _context;
+        public LancheRepository(AppDbContext contexto)
+        {
+            _context = contexto;
+        }
+        public IEnumerable<Lanche> Lanches => _context.Lanches.Include(c => c.Categoria);
+
+        public IEnumerable<Lanche> LanchesPreferidos => _context.Lanches.
+            Where(l => l.IsLanchePreferido).Include(c => c.Categoria);
+
+        public Lanche GetLancheById(int lancheid)
+        {
+            return _context.Lanches.FirstOrDefault(l => l.LancheId == lancheid);
+        }
+    }
+}
