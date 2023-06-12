3febe392921e591e3ef3c30256bcc8956736bcd3
Henrick Melo Vital <henrick.melo@yahoo.com.br>
2023-06-12 11:05:27 -0300

    Criando o Controller a View para exibir os lanches

---
+++ MVC_2022/Controllers/LancheController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVC_2022.Repositories.Interface;
+
+namespace MVC_2022.Controllers
+{
+    public class LancheController : Controller
+    {
+
+        private readonly ILancheRepository _lancheRepository;
+
+        public LancheController(ILancheRepository lancheRepository)
+        {
+            _lancheRepository = lancheRepository;
+        }
+
+        public IActionResult List()
+        {
+            var lanches = _lancheRepository.Lanches;
+            return View(lanches);
+        }
+    }
+}
