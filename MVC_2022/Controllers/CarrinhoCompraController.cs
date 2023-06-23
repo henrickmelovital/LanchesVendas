7822e0b7e911b3a5c4547253c49ec97d9a610502
Henrick Melo Vital <henrick.melo@yahoo.com.br>
2023-06-23 11:56:26 -0300

    Criando o Controlador para gerenciar os itens do carrinho de compras - II

--- MVC_2022/Controllers/CarrinhoCompraController.cs
+++ MVC_2022/Controllers/CarrinhoCompraController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using MVC_2022.Models;
 using MVC_2022.Repositories.Interface;
+using MVC_2022.ViewModels;
 
 namespace MVC_2022.Controllers
 {
@@ -19,7 +20,41 @@ namespace MVC_2022.Controllers
 
         public IActionResult Index()
         {
-            return View();
+            // Exibe o carrinho de compras:
+            var itens = _carrinhoCompra.GetCarrinhoCompraItens();
+
+            _carrinhoCompra.CarrinhoCompraItems = itens;
+
+            var carrinhoCompraVM = new CarrinhoCompraViewModel
+            {
+                CarrinhoCompra = _carrinhoCompra,
+                CarrinhoCompraTotal = _carrinhoCompra.GetCarrinhoCompraTotal()
+            };
+            return View(carrinhoCompraVM);
+        }
+
+        public IActionResult AdicionarItemNoCarrinhoCompra(int lancheId)
+        {
+            var lancheSelecionado = _lancheRepository.Lanches.FirstOrDefault(p => p.LancheId == lancheId);
+
+            if (lancheSelecionado != null)
+            {
+                _carrinhoCompra.AdicionarAoCarrinho(lancheSelecionado);
+            }
+
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult RemoverItemDoCarrinhoCompra(int lancheId)
+        {
+            var lancheSelecionado = _lancheRepository.Lanches.FirstOrDefault(P => P.LancheId == lancheId);
+
+            if (lancheSelecionado != null)
+            {
+                _carrinhoCompra.RemoverDoCarrinho(lancheSelecionado);
+            }
+
+            return RedirectToAction("Index");
         }
     }
 }
