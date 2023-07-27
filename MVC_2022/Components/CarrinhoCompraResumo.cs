75626d6b702bb189539946524f34183633f21a37
Henrick Melo Vital <henrick.melo@yahoo.com.br>
2023-07-27 20:49:43 -0300

    Criando uma ViewComponent para exibir detalhes do carrinho

---
+++ MVC_2022/Components/CarrinhoCompraResumo.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVC_2022.Models;
+using MVC_2022.ViewModels;
+
+namespace MVC_2022.Components
+{
+    public class CarrinhoCompraResumo : ViewComponent
+    {
+        private readonly CarrinhoCompra _carrinhoCompra;
+
+        public CarrinhoCompraResumo(CarrinhoCompra carrinhoCompra)
+        {
+            _carrinhoCompra = carrinhoCompra;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            //var itens = _carrinhoCompra.GetCarrinhoCompraItens();
+            var itens = new List<CarrinhoCompraItem>()
+            {
+                new CarrinhoCompraItem(),
+                new CarrinhoCompraItem()
+            };
+            _carrinhoCompra.CarrinhoCompraItems = itens;
+
+            var carrinhoCompraVM = new CarrinhoCompraViewModel
+            {
+                CarrinhoCompra = _carrinhoCompra,
+                CarrinhoCompraTotal = _carrinhoCompra.GetCarrinhoCompraTotal()
+            };
+            return View(carrinhoCompraVM);
+        }
+
+    }
+}
